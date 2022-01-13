8cd238393eb7ee4d7b66f0b921d06aed9bebf332
Marquzano <angel.angelm.marquez3@gmail.com>
2022-01-13 10:49:24 -0600

    Demonstrates the different ways of accesing items from a list

--- bicycles.py
+++ bicycles.py
@@ -1,9 +1,14 @@
 #This is an example of a list and it's output when printed
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
+print("\n")
 
 #Lists are ordered so you can access individual items from the list by using its index
-print(bicycles[0])
+print(bicycles[0] + "\n")
 
 #Depending on the data in the lists certain methods can be performed on the list if it is indexed
-print(bicycles[1].title())
+print(bicycles[1].title() + "\n")
+
+#The index starts at 0 so the fourth item in the list would be index 3
+#Also python can access the last items in a list with negative numbers 
+print(bicycles[1] + "\n" + bicycles[3] + "\n" + bicycles[-1] + "\n" + bicycles[-2])
