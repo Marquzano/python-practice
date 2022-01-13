310402efb0ac571545832eea308acf9411e997fb
Marquzano <angel.angelm.marquez3@gmail.com>
2022-01-13 10:40:09 -0600

    Showcases the ordered property of lists

--- bicycles.py
+++ bicycles.py
@@ -1,2 +1,6 @@
+#This is an example of a list and it's output when printed
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
+
+#Lists are ordered so you can access individual items from the list by using its index
+print(bicycles[0])
