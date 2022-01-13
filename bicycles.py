771d108b84708fda9a69d3d39b48038113cc9199
Marquzano <angel.angelm.marquez3@gmail.com>
2022-01-13 10:43:02 -0600

    Shows the ability to use methods on the list if indexed

--- bicycles.py
+++ bicycles.py
@@ -4,3 +4,6 @@ print(bicycles)
 
 #Lists are ordered so you can access individual items from the list by using its index
 print(bicycles[0])
+
+#Depending on the data in the lists certain methods can be performed on the list if it is indexed
+print(bicycles[1].title())
