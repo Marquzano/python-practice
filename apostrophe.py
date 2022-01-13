ff7a882192b23baaf5e82bb549d28a9a60a3e649
Marquzano <angel.angelm.marquez3@gmail.com>
2022-01-13 09:52:25 -0600

    Added comments
    This demonstrates the utility of comments

--- apostrophe.py
+++ apostrophe.py
@@ -1,2 +1,4 @@
+# Angel Marquez 1/13/2022
+# This script demonstrates a syntax error vulnerability due to the apostrophe in the sentence
 message = "One of Python's strengths is its diverse community."
 print(message)
--- favorite_number.py
+++ favorite_number.py
@@ -1,3 +1,5 @@
+# Angel Marquez 1/13/2022
+# This script is to show how to concatanate an integer to a string using the str wrapper
 fav_number = 4
 message = "My favorite number is:\n\t" + str(fav_number)
 print(message)
