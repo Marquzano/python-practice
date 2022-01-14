81379d15b98f1c9b4e14907e37a078f4f2543a35
Marquzano <angel.angelm.marquez3@gmail.com>
2022-01-14 10:53:59 -0600

    Included the sorted function to the script

--- Chapter 3/cars.py
+++ Chapter 3/cars.py
@@ -3,3 +3,16 @@
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 cars.sort()
 print(cars)
+
+#Introducing the sorted method
+#This temporarily sorts your list but doesn't affect the actual order of the list
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
