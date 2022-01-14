6e74d11664212386b4778347e809ef139fe79f75
Marquzano <angel.angelm.marquez3@gmail.com>
2022-01-14 10:44:31 -0600

    Changed a word in a sentence
    in friends_names.py

--- Chapter 3/Exercises/friends_names.py
+++ Chapter 3/Exercises/friends_names.py
@@ -1,7 +1,7 @@
 #This script is to practice with lists
 friends = ['Katherine', 'Jose', 'Charis', 'Jared', 'Ryan', 'Emily', 'Greg', 'Shiyi', 'Jordan', 'Kevin', 'Dylan', 'Emily S']
 
-message = "These are my friends (in no specific order):\n\t" + friends[0] + "\n\t" + friends[1] + "\n\t" + friends[2] + "\n\t" + friends[3] + "\n\t" + friends[4] + "\n\t" + friends[5] + "\n\t" + friends[6] + "\n\t" + friends[7] + "\n\t" + friends[8] + "\n\t" + friends[9] + "\n\t" + friends[10] + "\n\t" + friends[11] + "\n\t"
+message = "These are my friends (in no particular order):\n\t" + friends[0] + "\n\t" + friends[1] + "\n\t" + friends[2] + "\n\t" + friends[3] + "\n\t" + friends[4] + "\n\t" + friends[5] + "\n\t" + friends[6] + "\n\t" + friends[7] + "\n\t" + friends[8] + "\n\t" + friends[9] + "\n\t" + friends[10] + "\n\t" + friends[11] + "\n\t"
 
 print(message)
 
