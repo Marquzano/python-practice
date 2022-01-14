0a1c608eca5724c7c2805ebf6306d87b36c2421e
Marquzano <angel.angelm.marquez3@gmail.com>
2022-01-14 10:49:19 -0600

    Created script to introduce the sort method

---
+++ Chapter 3/cars.py
@@ -0,0 +1,5 @@
+#An introduction to sorting lists using the sort method
+#You cannot revert to the original order after calling this method
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
