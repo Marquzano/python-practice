e9d1d5ad62645bced66cbe684a9edc27e54571e4
Marquzano <angel.angelm.marquez3@gmail.com>
2022-01-15 15:01:20 -0600

    Completed an exercise using lists and loops

---
+++ Chapter_4/Exercises/pizzas.py
@@ -0,0 +1,9 @@
+#These pizzas are from Two Boots pizza Co.
+
+pizzas = ['vegan larry tate', 'earth mother', 'v for vegan']
+
+#This prints my favorite pizzas from the list
+for pizza in pizzas:
+    print("I like the " + pizza.title() + " pizza at Two Boots")
+
+print("I really like all kinds of pizza\nSince I've become vegan some things have changed\nBut I still really love pizza!!")
