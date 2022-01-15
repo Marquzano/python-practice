275fc262bd3f26735e4f37823dfbe1d82967d3af
Marquzano <angel.angelm.marquez3@gmail.com>
2022-01-15 15:39:17 -0600

    Added an example of list comprehension to squares.py

--- Chapter_4/squares.py
+++ Chapter_4/squares.py
@@ -5,3 +5,8 @@ for value in range(1,11):
     squares.append(value**2)
 
 print(squares)
+print('\n')
+
+#You can accomplish the above faster and easier in one line using list comprehension
+squares = [value**2 for value in range(1,11)]
+print(squares)
