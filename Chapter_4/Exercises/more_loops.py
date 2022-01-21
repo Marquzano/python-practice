59bb31e6e3b25dd7878a8f16ba9582bcbe43e64d
Marquzano <angel.angelm.marquez3@gmail.com>
2022-01-21 15:30:38 -0600

    Completed another exercise

---
+++ Chapter_4/Exercises/more_loops.py
@@ -0,0 +1,17 @@
+#This is another version of the foods.py script but with for loops
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print("My favorite foods are:")
+for food in my_foods:
+    print(food)
+print('\n')
+
+print("\nMy friend's favorite foods are:")
+for food in friend_foods:
+    print(food)
