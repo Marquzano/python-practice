48a3824df6deb38ca86b32987fe3cc49659408dd
Marquzano <angel.angelm.marquez3@gmail.com>
2022-01-31 15:28:40 -0600

    Added on more conditional statements to exercise

--- Chapter_5/Exercises/conditional_tests.py
+++ Chapter_5/Exercises/conditional_tests.py
@@ -20,6 +20,20 @@ print("\nIs p or l not in the predators name? I predict True")
 if 'p' not in team or 'l' not in team:
     print(True)
 
+guess = "PREDATORS"
+print("\nIs PREDATORS the same as predators? I predict True.")
+if guess.lower() == team:
+    print(True)
+
+age = 24
+print("\nIs age greater than 21? I predict True.")
+if age > 21:
+    print(True)
+
+print("\nIs age greater than or equal to 24? I predict True.")
+if age >= 24:
+    print(True)
+
 #******************************************************************
 
 print("\nIs team == 'blackhawks'? I predict False.")
@@ -47,5 +61,24 @@ else:
 print("\nIs p or e not in the predators name? I predict False")
 if 'p' not in team or 'e' not in team:
     print("Whaaat??")
+else:
+    print(False)
+
+guess = "BLACKHAWKS"
+print("\nIs BLACKHAWKS the same as predators? I predict False.")
+if guess.lower() == team:
+    print("This is awfully surprising?")
+else:
+    print(False)
+
+print("\nIs age less than 21? I predict False.")
+if age < 21:
+    print("This is unsettling.")
+else:
+    print(False)
+
+print("\nIs age less than or equal to 23? I predict False.")
+if age <= 23:
+    print("This is also unsettling.")
 else:
     print(False)
