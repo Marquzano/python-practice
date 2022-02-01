1e5d3379e1ebd10b70c79a0a7bda76142d47e5e6
Marquzano <angel.angelm.marquez3@gmail.com>
2022-02-01 12:59:09 -0600

    Added an exercise that uses a series of if statements to determine all of my favorite fruits

---
+++ Chapter_5/Exercises/favorite_fruit.py
@@ -0,0 +1,14 @@
+#Here we will show the use of equality conditions for a series of if statements
+
+favorite_fruits = ['mangos', 'apples', 'bananas']
+
+if 'mangos' in favorite_fruits:
+    print("You really like mangos!")
+if 'apples' in favorite_fruits:
+    print("You really like apples!")
+if 'bananas' in favorite_fruits:
+    print("You really like bananas!")
+if 'kiwis' in favorite_fruits:
+    print("You really like kiwis!")
+if 'dragonfruits' in favorite_fruits:
+    print("You really like dragonfruits!")
