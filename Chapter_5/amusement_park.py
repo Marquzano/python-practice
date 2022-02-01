6b0adaf8b243bc8f4d5f226ac9fbaa4c97e33287
Marquzano <angel.angelm.marquez3@gmail.com>
2022-02-01 12:07:31 -0600

    Added script which shows the functionality of if statements further

---
+++ Chapter_5/amusement_park.py
@@ -0,0 +1,38 @@
+#Often you'll need to test more than two possible situations
+#To evaluate these we can use Python's if-elif-else syntax
+
+age = 12
+
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $5.")
+else:
+    print("Your admission cost is $10.")
+print("\n")
+
+#In this situation we could just set the price and output the value in a single print
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+else:
+    price = 10
+
+print("Your admission cost is $" + str(price) + ".")
+print("\n")
+
+#You can use multiple elif (else if) statements as a chain
+
+age = 66
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+elif age < 65:
+    price = 10
+else:
+    price = 5
+
+print("Your admission cost is $" + str(price) + ".")
