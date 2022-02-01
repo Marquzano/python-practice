a9408a8676549a268eaab3ecbae69bcb4eb4aa76
Marquzano <angel.angelm.marquez3@gmail.com>
2022-02-01 16:52:16 -0600

    Finished another exercise using for loop and if statement chain

---
+++ Chapter_5/Exercises/ordinal_numbers.py
@@ -0,0 +1,12 @@
+#Another simple script combining the use of for loops and if statements
+numbers = range(1,10)
+
+for number in numbers:
+    if number == 1:
+        print(str(number) + "st")
+    elif number == 2:
+        print(str(number) + "nd")
+    elif number == 3:
+        print(str(number) + "rd")
+    else:
+        print(str(number) + "th")
