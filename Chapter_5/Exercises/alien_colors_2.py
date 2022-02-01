19bcc875b566d4644cc1d71a288a9c72d8db593f
Marquzano <angel.angelm.marquez3@gmail.com>
2022-02-01 12:36:05 -0600

    Finished another exercise that shows the use of if-else blocks

---
+++ Chapter_5/Exercises/alien_colors_2.py
@@ -0,0 +1,9 @@
+#This is another example of if statements but with the use of if-else conditions
+
+# alien_color = 'green' #This will execute the if block
+alien_color = 'yellow' #This will execute the else block
+
+if alien_color.lower() == 'green':
+    print("You recieved 5 points for shooting a green alien!")
+else:
+    print("You recieved 10 points for shooting a tougher alien!")
