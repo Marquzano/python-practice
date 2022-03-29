6d8de6bb714f9fac1a18b201b90917cf2a31bdb1
Marquzano <angel.angelm.marquez3@gmail.com>
2022-03-29 14:00:43 -0500

    Sample code and tests for phonebook problem

---
+++ Hackerrank_code/phonebook.py
@@ -0,0 +1,25 @@
+phonebook = {}
+    
+# this function adds n entries of names and corresponding phone numbers
+def entry(n):
+    for i in range(n):
+        name, number = input().split(' ')
+        phonebook.update({name: number})
+
+entry(3)
+
+print(phonebook)
+
+# this next function searches the number of names given
+# it gives the key value pair
+# if the name is not in the phonebook then it returns Not Found
+def search(n):
+    for i in range(n):
+        name = input()
+        if name not in phonebook:
+            print("Not Found")
+        else:
+            number = phonebook.get(name)
+            print(name + "=" + number)
+
+search(4)        
