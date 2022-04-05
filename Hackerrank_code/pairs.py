393d28cde072fa083e2b73ffaa67b175d9d80a5d
Marquzano <angel.angelm.marquez3@gmail.com>
2022-04-05 17:36:05 -0500

    Did the following: Came up with a working solution for pairs challenge Included code that was written for google's technical assessment

--- Hackerrank_code/pairs.py
+++ Hackerrank_code/pairs.py
@@ -1,23 +1,50 @@
 # what I have so far for the pairs problem
+# ok so I believe we could still iterate through the original array
+# it's the array we iterate over in the second for loop that needs to change
+
 
 def pairs(k, arr):
     # make a pairs variable
     pairs = 0
     # begin by subtracting each possible pair
-    print(arr)
-    # I may need to do this with indexes to keep it correct
-    for first in arr:
-        print("First(before difference for loop): " + str(first))
-        for second in arr[1:]:
-            print("Second: " + str(second))
-            diff = abs(first - second)
+    for i in range(len(arr)):
+        diffArr = arr[i+1:]
+        for j in range(len(diffArr)):
+            diff = abs(arr[i] - diffArr[j])
             if diff == k:
                 pairs += 1
-        print("First(after difference for loop): " + str(first))
-        # arr.remove(first)
     return pairs
 
-k = 2
+k = 3
 arr = [1,5,3,4,2]
 
 print(pairs(k, arr))
+
+# reference code from below to come up with an idea
+# for adjusting the size of the comparison array
+# according to the index of the first array
+# I will need to adjust the loops from above to work through an index
+# Bonus Note is that it was suggested that you could compare with greater ease using dictionaries
+# google coding interview
+
+# pair = False
+# numDiff = 0
+# for i in range(len(arr)):
+#   # second for loop is to compare
+#   comparisonArr = arr[i+1:]
+#   for j in range(len(comparisonArr)):
+#     firstStr = arr[i]
+#     secondStr = comparisonArr[j]
+#     # another for loop to compare each character in the string
+#     for i in range(m):
+#       if firstStr[i] == secondStr[i]
+#         continue
+#       elif firstStr[i] != secondStr[i]
+#         numDiff += 1
+
+#     if numDiff == 1:
+#       pair = True
+#       return pair
+#     else:
+#       pair = False
+#     numDiff = 0
