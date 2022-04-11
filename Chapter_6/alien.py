6d9ae90a95c3ab84d7d4e335da97228bf6f583ee
Marquzano <angel.angelm.marquez3@gmail.com>
2022-04-11 10:42:34 -0500

    Made more progress in crashcourse chapter 6

--- Chapter_6/alien.py
+++ Chapter_6/alien.py
@@ -24,3 +24,24 @@ alien_0['color'] = 'green'
 alien_0['points'] = 5
 
 print(alien_0)
+print('\n')
+
+# to modify a value in a dictionary, give the name of the dictionary with the key
+# in square brackets and then the new value you want associated with that key
+
+alien_0 = {'color': 'green'}
+print("The alien is " + alien_0['color'] + ".")
+
+alien_0['color'] = 'yellow'
+print("The alien is now " + alien_0['color'] + ".")
+print('\n')
+
+# when you no longer need a piece of information that's stored in a dictionary,
+# you can use the del statement to completely remove a key-value pair.
+# as shown below
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
