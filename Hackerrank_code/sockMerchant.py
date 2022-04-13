8e143e3833edd62c96ab9efaa4c686750a56e2b6
Marquzano <angel.angelm.marquez3@gmail.com>
2022-04-13 10:04:01 -0500

    Completed the sockMerchant challenge

--- Hackerrank_code/sockMerchant.py
+++ Hackerrank_code/sockMerchant.py
@@ -1,31 +1,37 @@
 # this challenge requires us to take
 # input n, the number of socks in a pile,
-# and input arr[n], wich is the array of colors
+# and input arr[n], which is the array of colors
 # we must return the number of pairs as an int
 
-def arrayToDict(arr):
-    sampleDict = {}
-    for i in range(len(arr)):
-        sampleDict.update({i+1: arr[i]})
-    return sampleDict
-
-def flip(dict):
-    flipped = {}
-    for key, value in dict.items():
-        if value not in dict:
-            flipped[value] = [key]
-        else:
-            flipped[value].append(key) # this line is giving me KeyError: 1
-    return flipped
+# the solution I am attempting to implement here is to
+# take the array of socks and get the unique colors
+# this set will then be the keys to a dictionary
+# the values for the keys will be the number of socks that
+# are that color in the array
 
 def sockMerchant(n, arr):
+    # set pairs to 0
     pairs = 0
-    sockDict = arrayToDict(arr)
-    flippedSockDict = flip(sockDict)
-    return flippedSockDict
+    # here we get the unique colors of the pile
+    uniqueColors = set(arr)
+    # make an empty dicitonary to be populated with data
+    sortedPile = {}
+    # first set the keys in the dictionary
+    for i in uniqueColors:
+        sortedPile[i] = 0
+    # now we sort them out in the dictionary
+    for color in sortedPile:
+        # here we go through the pile
+        for j in range(n):
+            # if the color is equal to the color then increment
+            if color == arr[j]:
+                sortedPile[color] += 1
+    # now that we have them sorted out
+    # we can look at the sortedPile and determine the number of pairs per color
+    for sameColor in sortedPile.values():
+        pairs += sameColor // 2
+    return pairs
 
 sampleArr = [1,2,1,2,1,3,2]
-
-print("sampleArray: ", str(sampleArr))
-print("Dictionary of sampleArray: ", str(arrayToDict(sampleArr)))
-print("The flipped version of the dictionary: ", str(sockMerchant(7, sampleArr)))
+samplePairs = sockMerchant(len(sampleArr), sampleArr)
+print(samplePairs)
