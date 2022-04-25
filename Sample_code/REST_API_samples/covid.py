8f124527989920e21c2c8fcf89b9c33f0c51e869
Marquzano <angel.angelm.marquez3@gmail.com>
2022-04-25 16:35:09 -0500

    Made changes to covid.py so that we can see the most recovered amongst the responses in a given day

--- Sample_code/REST_API_samples/covid.py
+++ Sample_code/REST_API_samples/covid.py
@@ -1,7 +1,9 @@
 # with this API I could begin doing my own work on modeling the progression of the covid-19 pandemic :o
 
+from time import time
 import requests
 import configparser
+import json
 
 # needed to parse ENVIRONMENT variables from config.txt
 config = configparser.ConfigParser()
@@ -24,26 +26,47 @@ headers = {
     "X-RapidAPI-Key": config['covid']['API_KEY'],
 }
 
-def printHistory(histResponse):
-    while histResponse:
-        data = histResponse.pop()
-        for key, value in data.items():
-            if type(value) == dict:
-                print("\n" + key.title() + ": {")
-                for key, values in value.items():
-                    print("\t\t" + key.title() + ": " + str(values))
-                print("}")
-            else:
-                print("\n" + key.title() + ": " + str(value))
-        print("\n\n")
+# the dumps() method makes my printHistory function obsolete...
+# def printHistory(histResponse):
+#     while histResponse:
+#         data = histResponse.pop()
+#         for key, value in data.items():
+#             if type(value) == dict:
+#                 print("\n" + key.title() + ": {")
+#                 for key, values in value.items():
+#                     print("\t\t" + key.title() + ": " + str(values))
+#                 print("}")
+#             else:
+#                 print("\n" + key.title() + ": " + str(value))
+#         print("\n\n")
 
 
 history = requests.request('GET', histURL, headers=headers, params=histQuery).json()
 countryInfo = ''
 stats = ''
 
+text = json.dumps(history, sort_keys=True, indent=4)
+# print(text)
 # key: 'response' has all the info we need
-histResponse = history['response']
-printHistory(histResponse)
+# histResponse = history['response']
+# printHistory(histResponse)
 
 # figure out a way to print a months worth of data for a list countries
+
+# if I wanted to report on the new highest recovery count and give the time how would I do that?
+
+def timeOfMostRecovered(history):
+    timeMostRecovered = ''
+    histResponses = history['response']
+    while histResponses:
+        review = histResponses.pop()
+        reviewed = []
+        recovered = {}
+        recovered[review['cases']['recovered']] = review['time']
+        reviewed.append(review)
+    timeMostRecovered = recovered[max(recovered)]
+    return timeMostRecovered
+
+result = timeOfMostRecovered(history)
+print(result)
+
