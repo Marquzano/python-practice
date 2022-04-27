5893d5fe67e3f617587278c09d13ef27c2a13f5b
Marquzano <angel.angelm.marquez3@gmail.com>
2022-04-27 10:59:59 -0500

    Completed binary numbers challenge

--- Hackerrank_code/leading_ones_in_binary.py
+++ Hackerrank_code/leading_ones_in_binary.py
@@ -7,8 +7,28 @@ import re
 import sys
 
 def leadingOnes(binary):
-    """this will return the max number of leading ones in a binary number"""
-    # will continue on this challenge at a later time
+    """This will return the max number of leading ones in a binary number"""
+    # the variable count is supposed to keep up with consecutive ones in the binary sequence
+    # countArr is to append each count of consecutive ones in the sequence
+    count = 0
+    countArr = []
+    for i in range(len(binary)):
+        # here we increase the count for every consecutive one in the sequence
+        if binary[i] == 1:
+            count += 1
+            # this nested if is to help append the remaining count when we reach the end
+            # and can't append in the other cases
+            if i + 1 == len(binary) and binary[i] == 1:
+                countArr.append(count)
+        # once we encounter a zero we append the last count of consecutive ones
+        # then we reset the count
+        else:
+            countArr.append(count)
+            count = 0
+        # if the entire sequence is consecutive ones we append the count as such to the count array
+        if count == len(binary):
+            countArr.append(count)
+    return max(countArr)
 
 def convertToBinary(n):
     """Takes a base 10 integer and returns base 2 binary representation"""
@@ -18,15 +38,21 @@ def convertToBinary(n):
         bit = n % 2
         n = n//2
         binaryArr.append(bit)
-    binaryArr = binaryArr[::-1] # have to reverse the order
+    binaryArr = binaryArr[::-1] # have to reverse the order to be a proper binary sequence
     return binaryArr
 
 
 if __name__ == '__main__':
-    n = int(input().strip())
+    while True:
+        print("Please enter a number:")
+        n = input("(If you are done enter 'q' to quit) ").strip()
+        if n == 'q' or 'q' in n.lower():
+            break
+        n = int(n)
 
-    binaryN = convertToBinary(n)
+        binaryN = convertToBinary(n)
 
-    # print(str(binaryN))
+        print(str(binaryN))
 
-    result = leadingOnes(binaryN)
+        result = leadingOnes(binaryN)
+        print(result)
