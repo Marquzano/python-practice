67d1e8a2fc6c2de9e074413cf9fa463b9ef39ae8
Marquzano <angel.angelm.marquez3@gmail.com>
2022-05-05 10:40:36 -0500

    Learned more about representing real world objects in code models

--- Chapter_9/electric_car.py
+++ Chapter_9/electric_car.py
@@ -91,10 +91,11 @@ class ElectricCar(Car):
         self.battery_size = 70
     
     def describe_battery(self):
+        """Gives description of the car's battery"""
         print("This car has a " + str(self.battery_size) + "-kWh battery.")
 
     def fill_gas_tank(self):
-        """Electric car's don't have gas tanks."""
+        """Reminds us that electric car's don't have gas tanks."""
         print("This car doesn't need a gas tank!")
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
@@ -104,3 +105,58 @@ my_tesla.describe_battery()
 my_car = Car('ford', 'focus', 2019)
 my_car.fill_gas_tank()
 my_tesla.fill_gas_tank()
+print("\n")
+
+# when modeling something from the real world in code,
+# you may find that you're adding more and more detail
+# to a class. You might recognize that part of one class
+# can be written as a separate class. You can break your
+# large class into smaller classes that work together
+
+# in this case the variable battery in electrib car is
+# now an instance of the Battery class
+
+# we can now add more attributes and methods regarding
+# the battery without cluttering the electric car class
+
+class Battery():
+    """A simple attempt to model a battery for an electric car."""
+    def __init__(self, battery_size=70):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+    
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
+
+class ElectricCar(Car):
+    """Represents aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+    def fill_gas_tank(self):
+        """Reminds us that electric car's don't have gas tanks."""
+        print("This car doesn't need a gas tank!")
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
